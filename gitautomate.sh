afbf0435880f60402385ff52011b51a302091c8c
salmaan <sonuhulk8765@gmail.com>
2024-11-14 20:39:58 +0530

    done fixing bugs

--- gitautomate.sh
+++ gitautomate.sh
@@ -1,16 +1,14 @@
-#!/bin/bash
-
-echo adding files.......
+echo "adding files......."
 sleep 1
 git add .
 sleep 1
 
-echo commit
+echo "commit"
 read -p "Enter a commit msg: " cmt_msg 
 git commit -m "$cmt_msg" 
-echo comitted............
+echo "committed............"
 sleep 1
 
-echo pusing to repo..............
+echo "pushing to repo.............."
 git push
-echo Completed!!!
+echo "Completed!!!"
